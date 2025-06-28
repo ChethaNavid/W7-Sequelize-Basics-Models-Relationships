520c81cc0d48dcd5a6b35326ad03f59bfb8db8a1
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-06-28 19:43:30 +0700

    setup server and database

--- EX-2/src/server.js
+++ EX-2/src/server.js
@@ -1,8 +1,25 @@
 import sequelize from "./db/database.js";
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv'
+import seedData from "../seeders/seedAuthorBook.js";
+import authorRouter from "../routes/authorRouter.js";
+import bookRouter from "../routes/bookRouter.js";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 try {
     await sequelize.authenticate();
-    console.log("Successfully connected to the database");
+    // await seedData();  //Run this line once only! After running it once, comment it back
 } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.log("Unable to connect to database");
 }
+app.use('/api', authorRouter);
+app.use('/api', bookRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+})
